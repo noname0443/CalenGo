6474104e4f9225899eec3e7e2316e259148365fb
noname0443 <noname342341@gmail.com>
2024-03-13 14:19:21 +0300

    Init backend and frontend

---
+++ backend/internal/app.go
@@ -0,0 +1,32 @@
+package internal
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+)
+
+type App struct {
+	fiberApp *fiber.App
+	port     string
+}
+
+func NewApp(port string) *App {
+	app := &App{
+		fiberApp: fiber.New(),
+		port:     port,
+	}
+
+	app.fiberApp.Get("/", func(c *fiber.Ctx) error {
+		logrus.Info("Hello world!")
+		return c.SendString("Hello, World!")
+	})
+
+	return app
+}
+
+func (app *App) Run() {
+	err := app.fiberApp.Listen(app.port)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
